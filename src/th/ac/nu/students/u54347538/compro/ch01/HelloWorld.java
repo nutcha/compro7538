7e5f9637327e090e901e20a0a30d9f93b2fd9cc8
nutcha <nutcha.toei@hotmail.com>
2012-06-20 16:31:19 +0700

    สร้างHelloWorld

---
+++ src/th/ac/nu/students/u54347538/compro/ch01/HelloWorld.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package th.ac.nu.students.u54347538.compro.ch01;
+
+/**
+ *
+ * @author nutchahuntong
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
